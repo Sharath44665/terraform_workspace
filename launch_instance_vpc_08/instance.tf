52e5408ab14be51c7439387190eca850c47f1d24
Sharath-Win64 <sharathwelcomesyouin@gmail.com>
2022-07-25 14:31:15 +0530

    fix typo errors

--- launch_instance_vpc_08/instance.tf
+++ launch_instance_vpc_08/instance.tf
@@ -1,12 +1,15 @@
 resource "aws_instance" "example" {
-  ami="${lookup(var.AMIS, var.AWS_REGION)}"
+  ami           = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
+
+  # the VPC subnet
+  subnet_id = aws_subnet.main-public-1.id
+
+  # the security group
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id]
+
+  # the public SSH key
+  key_name = aws_key_pair.mykeypair.key_name
 }
 
-# ec2 instance subnet
-subnet_id = aws_subnet.main-public-1.id
-
-# security group
-vpc_security_group_ids = [aws_security_group.allow-ssh.id]
-
-key_name = aws_key_pair.mykeypair.key_name
+    
