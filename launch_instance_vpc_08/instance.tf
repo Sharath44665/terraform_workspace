e6c9992cbc0b942b965b2f7c75a501704758beab
Sharath-Win64 <sharathwelcomesyouin@gmail.com>
2022-07-25 13:29:06 +0530

    add ec2 instance

---
+++ launch_instance_vpc_08/instance.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "example" {
+  ami="${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type = "t2.micro"
+}
+
+# ec2 instance subnet
+subnet_id="${aws_subnet.main-public-1.id}"
+
+# security group
+vpc_security_group_ids=["${aws_security_group.allow-ssh.id}"]
+
+key_name="${aws_key_pair.mykeypair.key_name}"
