134594d9f952155437101c9fbbf6a627fca2188e
Sharath-Win64 <sharathwelcomesyouin@gmail.com>
2022-07-26 11:31:39 +0530

    add variables

---
+++ rds_12/vars.tf
@@ -0,0 +1,21 @@
+variable "AWS_REGION" {
+  default = "ap-south-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    ap-south-1 = "ami-068257025f72f470d"
+  }
+}
+
+variable "RDS_PASSWORD" {
+}
