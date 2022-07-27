a9a2933b44e1469da838329235431e2bb2006a8f
Sharath-Win64 <sharathwelcomesyouin@gmail.com>
2022-07-27 12:04:11 +0530

    change ami id

--- autoscaling_15/vars.tf
+++ autoscaling_15/vars.tf
@@ -13,8 +13,6 @@ variable "PATH_TO_PUBLIC_KEY" {
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+     ap-south-1 = "ami-068257025f72f470d"
   }
 }
